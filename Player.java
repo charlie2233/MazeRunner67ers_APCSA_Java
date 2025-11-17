3d2c4c881555658a53c91add03eb2f99eec245bc
CharLEE.cpp <117957000+charlie2233@users.noreply.github.com>
2025-11-17 19:49:24 +0000

    feat: player movement

--- Player.java
+++ Player.java
@@ -6,11 +6,11 @@ import java.util.*;
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Player extends actor
+public class Player extends Actor
 {
     // instance variables - replace the example below with your own
     private int x = 0,y = 0;// change later
-    private mapAura;
+    private mapAura mapAura;
     private static final int SPEED = 5;
 
     /**
@@ -18,7 +18,7 @@ public class Player extends actor
      */
     public Player()
     {
-        mapAura = new mapAura()
+        mapAura = new mapAura();
         GreenfootImage img67ers = new GreenfootImage("accesories/" + "siz_Seven_Kid.jpg");//fixed routes
         setImage(img67ers);
     }
@@ -29,23 +29,26 @@ public class Player extends actor
     //  * @param  y   a sample parameter for a method
     //  * @return     the sum of x and y 
     //  */int sampleMethod
-    public void act()
+    public void act() // right/left/up/down movement (speed constant)
     {
         if (Greenfoot.isKeyDown("right")) {
-            // Move the actor to the right by a certain amount (e.g., 3 pixels)
-            setLocation(getX() + SPEED, getY());
-        }else if (Greenfoot.isKeyDown("left")) {
-            // Move the actor to the right by a certain amount (e.g., 3 pixels)
-            setLocation(getX()-SPEED, getY());
-        }else if (Greenfoot.isKeyDown("up")) {
-            // Move the actor to the right by a certain amount (e.g., 3 pixels)
-            setLocation(getX(), getY()+SPEED);
-        }else if (Greenfoot.isKeyDown("down")) {
-            // Move the actor to the right by a certain amount (e.g., 3 pixels)
-            setLocation(getX(), getY()-SPEED);
+            if (mapAura.get(getX() + SPEED, getY()) == false) // check if wall
+                setLocation(getX() + SPEED, getY());
+        } else if (Greenfoot.isKeyDown("left")) {
+            if (mapAura.get(getX() - SPEED, getY()) == false) 
+                setLocation(getX() - SPEED, getY());
+        } else if (Greenfoot.isKeyDown("up")) {
+            if (mapAura.get(getX(), getY() - SPEED) == false)
+                setLocation(getX(), getY() - SPEED);
+        } else if (Greenfoot.isKeyDown("down")) {
+            if (mapAura.get(getX(), getY() + SPEED) == false) 
+                setLocation(getX(), getY() + SPEED);
         }
     }
 }
+
+//example code from greendfoot docs for movement
+// // Check if the right arrow key is pressed
 // if (Greenfoot.isKeyDown("right")) {
 //     // Move the actor to the right by a certain amount (e.g., 3 pixels)
 //     setLocation(getX() + 3, getY());
